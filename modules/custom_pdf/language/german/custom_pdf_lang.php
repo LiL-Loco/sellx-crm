d0fb674b41263bdbc4bba6470cd9e0749c77b86b
LiL-Loco <38622995+LiL-Loco@users.noreply.github.com>
2024-12-10 21:03:09 +0100

    :tada:

---
+++ modules/custom_pdf/language/german/custom_pdf_lang.php
@@ -0,0 +1,68 @@
+<?php
+
+defined('BASEPATH') || exit('Kein direkter Zugriff erlaubt');
+
+// Modulname
+$lang['custom_pdf'] = 'Benutzerdefiniertes PDF';
+
+// Cover-Seite
+$lang['cover_page'] = 'Titelseite';
+$lang['cover_page_subtext'] = 'Gestalten und personalisieren Sie die Einleitungsseite Ihres PDFs.';
+$lang['cover_image'] = 'Titelbild';
+$lang['cover_page_text'] = 'Text für die Titelseite';
+$lang['cover_image_tooltip'] = 'Laden Sie ein Bild hoch, das als Titelbild des PDFs angezeigt wird.';
+$lang['cover_page_text_tooltip'] = 'Geben Sie den Text ein, der auf der Titelseite des PDFs angezeigt werden soll.';
+$lang['recommanded_dimensions'] = 'Empfohlene Abmessungen:';
+
+// Kopfzeile
+$lang['header'] = 'Kopfzeile';
+$lang['header_subtext'] = 'Passen Sie den Inhalt der Kopfzeile für Ihr PDF-Dokument an.';
+$lang['header_image'] = 'Kopfzeilenbild';
+$lang['header_text'] = 'Kopfzeilentext';
+$lang['header_image_tooltip'] = 'Laden Sie ein Bild für die Kopfzeile Ihres PDFs hoch.';
+$lang['header_text_tooltip'] = 'Geben Sie den Text ein, der in den Kopfzeilen des PDFs angezeigt werden soll.';
+
+// Fußzeile
+$lang['footer'] = 'Fußzeile';
+$lang['footer_subtext'] = 'Passen Sie den Inhalt der Fußzeile an die spezifischen Anforderungen Ihres PDFs an.';
+$lang['footer_image'] = 'Fußzeilenbild';
+$lang['footer_text'] = 'Fußzeilentext';
+$lang['footer_image_tooltip'] = 'Laden Sie ein Bild für die Fußzeile Ihres PDFs hoch.';
+$lang['footer_text_tooltip'] = 'Geben Sie den Text ein, der in den Fußzeilen des PDFs angezeigt werden soll.';
+
+// Abschlussseite
+$lang['closing_page'] = 'Abschlussseite';
+$lang['closing_page_subtext'] = 'Erstellen Sie eine individuell gestaltete Abschlussseite für Ihre PDF-Datei.';
+$lang['closing_image'] = 'Abschlussbild';
+$lang['closing_page_text'] = 'Text für die Abschlussseite';
+$lang['closing_image_tooltip'] = 'Laden Sie ein Bild hoch, das als Abschlussbild des PDFs angezeigt wird.';
+$lang['closing_page_text_tooltip'] = 'Geben Sie den Text ein, der auf der Abschlussseite des PDFs angezeigt werden soll.';
+
+// Allgemein
+$lang['preview_image'] = 'Bildvorschau';
+$lang['remove'] = 'Bild entfernen';
+$lang['align_from_top'] = 'Ausrichtung von oben (in px)';
+$lang['align_from_top_help'] = 'Geben Sie die Anzahl der Pixel ein, um die Ausrichtung von %s von oben festzulegen.';
+$lang['align_from_left'] = 'Ausrichtung von links (in px)';
+$lang['align_from_left_help'] = 'Geben Sie die Anzahl der Pixel ein, um die Ausrichtung von %s von links festzulegen.';
+$lang['pdf_sections'] = '%s PDF-Bereiche';
+$lang['preview'] = 'Vorschau';
+$lang['allowed_extensions'] = 'Erlaubte Erweiterungen sind (.png, .jpg, .jpeg)';
+$lang['custom_pdf_settings'] = 'Einstellungen für benutzerdefinierte PDFs';
+
+// Seit Version 1.0.1
+$lang['items_table'] = 'Elementetabelle';
+$lang['bg_color_tooltip'] = 'Hintergrundfarbe für diese Tabellenzeile festlegen';
+$lang['text_color_tooltip'] = 'Textfarbe für diese Tabellenzeile festlegen';
+$lang['heading_section'] = 'Kopfzeilenzeile';
+$lang['odd_rows_section'] = 'Ungerade Zeilen';
+$lang['even_rows_section'] = 'Gerade Zeilen';
+$lang['total_row_section'] = 'Summenzeile';
+$lang['bg_color'] = 'Hintergrundfarbe';
+$lang['text_color'] = 'Textfarbe';
+$lang['row_section'] = 'Andere Zeilen';
+$lang['heading_row'] = 'Kopfzeilenzeile';
+$lang['odd_row'] = 'Ungerade Zeile';
+$lang['even_row'] = 'Gerade Zeile';
+$lang['total_row'] = 'Summenzeile';
+$lang['other_row'] = 'Andere Zeilen';
